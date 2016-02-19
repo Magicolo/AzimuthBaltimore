33eb60170eb3fbae7b77453cdc6ddcd9993f2d35
Magicolo <straty91@hotmail.com>
2016-02-19 18:32:10 -0500

    -dummy level -Player motion in za worx

---
+++ Assets/Scripts/Components/InputMotion.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using Pseudo;
+
+public class InputMotion : ComponentBehaviour
+{
+	public float Speed = 5f;
+	public Players Player = Players.Player1;
+	public string Horizontal = "MoveX";
+	public string Vertical = "MoveY";
+	public TimeComponent Time;
+	public Rigidbody Rigidbody;
+
+	Vector3 motion;
+
+	[Inject]
+	readonly IInputManager inputManager = null;
+
+	void Update()
+	{
+		motion.x = inputManager.GetAxis(Player, Horizontal) * Speed;
+		motion.z = inputManager.GetAxis(Player, Vertical) * Speed;
+	}
+
+	void FixedUpdate()
+	{
+		Rigidbody.Translate(motion * Time.FixedDeltaTime, Axes.XZ);
+	}
+}
