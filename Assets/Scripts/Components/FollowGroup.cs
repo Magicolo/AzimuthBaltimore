6d4ae37ed5b1e68f4b9d2711b00745255b45ac50
Magicolo <straty91@hotmail.com>
2016-02-19 18:53:47 -0500

    Player motion Camera motion in za worx

---
+++ Assets/Scripts/Components/FollowGroup.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using Pseudo;
+
+public class SeekTarget : ComponentBehaviour
+{
+	public Vector3 Speed = new Vector3(5f, 5f, 5f);
+	[Mask(Axes.XYZ)]
+	public Axes Axes = Axes.XYZ;
+	public TargetBase Target;
+	public TimeComponent Time;
+	public Transform Transform;
+}
