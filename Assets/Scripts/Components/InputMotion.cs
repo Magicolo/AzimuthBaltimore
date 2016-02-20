44ff20f22687a592b0d2ff2eb3f0e5fa3fd0f95e
Magicolo <straty91@hotmail.com>
2016-02-20 17:33:16 -0500

    Player visuals

--- Assets/Scripts/Components/InputMotion.cs
+++ Assets/Scripts/Components/InputMotion.cs
@@ -11,10 +11,14 @@ public class InputMotion : ComponentBehaviour
 	public Players Player = Players.Player1;
 	public string Horizontal = "MoveX";
 	public string Vertical = "MoveY";
+	public string AnimatorSpeed = "Speed";
+	public string AnimatorDirection = "Direction";
 	public TimeComponent Time;
 	public Rigidbody Rigidbody;
+	public Animator Animator;
 
 	Vector3 motion;
+	int lastDirection;
 
 	[Inject]
 	readonly IInputManager inputManager = null;
@@ -24,6 +28,23 @@ public class InputMotion : ComponentBehaviour
 		motion.x = inputManager.GetAxis(Player, Horizontal);
 		motion.z = inputManager.GetAxis(Player, Vertical);
 		motion = motion.normalized * Speed;
+
+		float speed = motion.magnitude;
+		int direction;
+
+		if (speed <= 0.1f)
+			direction = lastDirection;
+		else if (Mathf.Abs(motion.x) >= Mathf.Abs(motion.z))
+		{
+			direction = 0;
+			Animator.transform.SetLocalScale(motion.x.Sign(), Axes.X);
+		}
+		else
+			direction = motion.z.Sign();
+
+		Animator.SetFloat(AnimatorSpeed, speed);
+		Animator.SetInteger(AnimatorDirection, direction);
+		lastDirection = direction;
 	}
 
 	void FixedUpdate()
